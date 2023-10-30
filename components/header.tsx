79dc0630c2dc3867c47f8bba6abf94bd2d76be93
Gabriel Albano <albanogabriel33@gmail.com>
2023-10-29 23:23:38 -0300

    criando motion.li

--- components/header.tsx
+++ components/header.tsx
@@ -24,16 +24,18 @@ export default function Header() {
             justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500
             sm:w-[initial] sm:flex-nowrap sm:gap-5">
                 {links.map(link => (
-                    <li 
+                    <motion.li 
                     className="h-3/4 items-center justify-center" 
                     key={link.hash}
+                    initial={{ y: -100, opacity: 0}}
+                    animate={{ y: 0, opacity: 1}}
                     >
                         <Link 
                         className="flex w-full items-center
                         justify-center px-3 py-3
                         hover:text-gray-950"
                         href={link.hash}>{link.name}</Link>
-                    </li>
+                    </motion.li>
                 ))}
             </ul>
         </nav>
