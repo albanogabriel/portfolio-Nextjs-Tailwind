58ff961c410d4f37dd37e2e0f8e97acc8a7c16e4
Gabriel Albano <albanogabriel33@gmail.com>
2023-10-30 22:07:26 -0300

    add motion div e motion span

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,12 +1,22 @@
+"use client"
+
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export default function Intro() {
   return (
     <section className="flex items-center justify-center">
       <div className="relative">
-        <motion.div>
-          <Image 
+        <motion.div
+          initial={{ opacity: 0, scale: 0}}
+          animate={{ opacity:1, scale: 1}}
+          transition={{
+            type: 'tween',
+            duration: 0.2
+          }}  
+        >
+          <Image  
           src="https://avatars.githubusercontent.com/u/126198713?s=400&u=397261c66fadb7d649781e77343c3239d88ed556&v=4" 
           alt={''}
           width="250"
@@ -18,7 +28,16 @@ export default function Intro() {
           />
         </motion.div>
 
-        <span className="text-3xl bottom-0 right-[-0.2rem] absolute">👻</span>
+        <motion.span className="text-3xl bottom-0 right-[-0.2rem] absolute"
+          initial={{ opacity: 0, scale: 0}}
+          animate={{ opacity: 1, scale: 1}}
+          transition={{
+            type: 'spring',
+            stiffness: 125,
+            delay: 0.1,
+            duration: 0.7
+          }}
+        >👻</motion.span>
       </div>
     </section>
   )
