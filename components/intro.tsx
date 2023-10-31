7b15aa761e311ffbe817fbeb18a328e856a92b47
Gabriel Albano <albanogabriel33@gmail.com>
2023-10-31 09:42:38 -0300

    fix: add div intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,38 +6,40 @@ import { motion } from 'framer-motion'
 
 export default function Intro() {
   return (
-    <section className="flex items-center justify-center">
-      <div className="relative">
-        <motion.div
-          initial={{ opacity: 0, scale: 0}}
-          animate={{ opacity:1, scale: 1}}
-          transition={{
-            type: 'tween',
-            duration: 0.2
-          }}  
-        >
-          <Image  
-          src="https://avatars.githubusercontent.com/u/126198713?s=400&u=397261c66fadb7d649781e77343c3239d88ed556&v=4" 
-          alt={''}
-          width="250"
-          height="250"
-          quality="95"
-          priority={true}
-          className="h-24 w-24 rounded-full object-cover border-[0.15rem]
-          border-white shadow-xl"
-          />
-        </motion.div>
+    <section >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0}}
+            animate={{ opacity:1, scale: 1}}
+            transition={{
+              type: 'tween',
+              duration: 0.2
+            }}  
+          >
+            <Image  
+            src="https://avatars.githubusercontent.com/u/126198713?s=400&u=397261c66fadb7d649781e77343c3239d88ed556&v=4" 
+            alt={''}
+            width="250"
+            height="250"
+            quality="95"
+            priority={true}
+            className="h-24 w-24 rounded-full object-cover border-[0.15rem]
+            border-white shadow-xl"
+            />
+          </motion.div>
 
-        <motion.span className="text-3xl bottom-0 right-[-0.2rem] absolute"
-          initial={{ opacity: 0, scale: 0}}
-          animate={{ opacity: 1, scale: 1}}
-          transition={{
-            type: 'spring',
-            stiffness: 125,
-            delay: 0.1,
-            duration: 0.7
-          }}
-        >👻</motion.span>
+          <motion.span className="text-3xl bottom-0 right-[-0.2rem] absolute"
+            initial={{ opacity: 0, scale: 0}}
+            animate={{ opacity: 1, scale: 1}}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7
+            }}
+          >👻</motion.span>
+        </div>
       </div>
     </section>
   )
